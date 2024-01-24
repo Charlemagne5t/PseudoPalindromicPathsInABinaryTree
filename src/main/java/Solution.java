29e928b6cf3351d1574111385fbdceb308685f73
alek <wait82985@gmail.com>
2024-01-24 13:50:46 +0600

    add bitmask logic

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,39 +1,29 @@
 class Solution {
     public int pseudoPalindromicPaths (TreeNode root) {
-        int[] count = new int[10];
+        int mask = 1 << root.val;
         int[] res = new int[1];
-        count[root.val]++;
-        dfs(root, count, res);
+        dfs(root, mask, res);
         return res[0];
 
     }
-    private void dfs(TreeNode root, int[] count, int[] res){
+    private void dfs(TreeNode root, int mask, int[] res){
         if(root.left == null && root.right == null){
-            boolean odd = false;
-            for(int i = 0; i < count.length; i++){
-                if(count[i] % 2 == 1){
-                    if(odd){
-                        return;
-                    }else odd = true;
-                }
+            if(Integer.bitCount(mask) < 2){
+                res[0]++;
             }
-            res[0]++;
             return;
         }
 
 
         if(root.left != null){
-            count[root.left.val]++;
-            dfs(root.left, count, res);
-            count[root.left.val]--;
+            mask ^= 1 << root.left.val;
+            dfs(root.left, mask, res);
+            mask ^= 1 << root.left.val;
         }
         if(root.right != null){
-            count[root.right.val]++;
-            dfs(root.right, count, res);
-            count[root.right.val]--;
+            mask ^= 1 << root.right.val;
+            dfs(root.right, mask, res);
         }
-
-
 
     }
 }
